da671cb6052dad0ebba421d0b22e3b4852ad6937
Ihor n-v <117747499+IgorIlyakhov@users.noreply.github.com>
2022-12-12 19:05:45 +0200

    Add files via upload

---
+++ dz16.py
@@ -0,0 +1,12 @@
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+
+
+def say_hi(x, y):
+    a = f"Hi. My name is {x} and I'm {y} years old."
+    return a
+
+
+b = say_hi("Alex", 32)
+
+print(b)
